a46c8d17e7878a01cb82e2fab90a659273b78717
Presy Lord Kidlat <91861180+presylord@users.noreply.github.com>
2021-12-03 06:31:26 +0800

    updateconditional rendering practice

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import Input from "./Input";
 
-function Form() {
+function Form(props) {
   return (
+    // <form className="form">
+    //   <input type="text" placeholder="Username" />
+    //   <input type="password" placeholder="Password" />
+    //   <input type="password" placeholder="Confirm Password" />
+    //   <button type="submit">Register</button>
+    // </form>
     <form className="form">
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <input type="password" placeholder="Confirm Password" />
-      <button type="submit">Register</button>
+      {props.status === false && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+      <button type="submit">{props.status ? "Login" : "Register"}</button>
     </form>
   );
 }
-
 export default Form;
